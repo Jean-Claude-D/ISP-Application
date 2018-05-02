8c038fa5ae3887bd142441cd6b08ddb4319b321d
DesJC <jcd9930@hotmail.ca>
2018-05-02 11:54:55 -0400

    Start InternetPackage model Class

---
+++ lib/InternetPackage.java
@@ -0,0 +1,35 @@
+public final class InternetPackage {
+	private String name;
+	public static final int MAX_NAME_LENGTH = 40;
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		if(isValidName(name)) {
+			this.name = name;
+		}
+		else {
+			throw new IllegalArgumentException("Cannot set name to :\n" + (name == null ? "null" : name) + "\nIs invalid name");
+		}
+	}
+	public static boolean isValidName(String name) {
+		return name != null && name.length() <= MAX_NAME_LENGTH;
+	}
+	
+	private String description;
+	public static final int MAX_DESCRIPTION_LENGTH = 250;
+	public String getDescription() {
+		return this.description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public static boolean isValidDescription(String description) {
+		return description == null || description.length() <= MAX_DESCRIPTION_LENGTH;
+	}
+	public boolean hasDescription() {
+		return this.description != null;
+	}
+	
+	
+}
