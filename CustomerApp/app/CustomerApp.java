5da130939bb06e41da5687a9a15ec28894400563
DesJC <jcd9930@hotmail.ca>
2018-05-09 00:23:41 -0400

    Add a connection utility class

--- CustomerApp/app/CustomerApp.java
+++ CustomerApp/app/CustomerApp.java
@@ -1,5 +1,7 @@
 package app;
 
+import lib.UserInputUtil;
+
 public final class CustomerApp {
 	private CustomerApp() {
 	}
@@ -8,7 +10,11 @@ public final class CustomerApp {
 		char userInput = 'q';
 		
 		do {
-			
+			userInput = UserInputUtil.getCharInput(
+				"Please enter your choice",
+				"Must be a valid character",
+				(c) -> {return true;}
+			);
 		}while(userInput != 'q');
 	}
 }
---
+++ CustomerApp/biz/ConnectionUtil.java
@@ -0,0 +1,24 @@
+package biz;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public final class ConnectionUtil {
+	public ConnectionUtil() {
+	}
+	
+	public Connection getConnection(String location, String user, String password) throws SQLException{
+		return DriverManager.getConnection("jdbc:oracle:thin:@" + location + ":1521:xe", user, password);
+	}
+	
+	public boolean testConnection(String location, String user, String password) {
+		try(Connection test = getConnection(location, user, password)) {
+			return test != null && test.isValid(0);
+		}
+		catch(SQLException exc) {
+			return false;
+		}
+	}
+}
