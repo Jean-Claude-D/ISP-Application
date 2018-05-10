4820456f76a401ddce9fe1be9bf1ed8c5ad77904
DesJC <jcd9930@hotmail.ca>
2018-05-09 20:40:12 -0400

    Changed to using cursor inseveral stored func/proc

--- CustomerApp/biz/InternetPackageUtil.java
+++ CustomerApp/biz/InternetPackageUtil.java
@@ -1,10 +1,17 @@
 package biz;
 
+import lib.ConnectionUtil;
+
 public final class InternetPackageUtil {
 	private InternetPackageUtil() {
 	}
 	
-	public static InternetPackage[] getPackagesFor(String username) {
-		
+	public static InternetPackage[] getPackagesFor(String username) throws SQLException {
+		try(Connection conn = ConnectionUtil.getConnection(false)) {
+			
+		}
+		catch(SQLException exc) {
+			throw exc;
+		}
 	}
 }
